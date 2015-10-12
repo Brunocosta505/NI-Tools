1bf031c36a9b5ab67b21bf8cb78ba50e9b7bb1fc
Bruno Costa <brunocosta505@hotmail.com>
2015-10-12 19:40:28 +0100

    Commit 12-10-2015
    New update

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 from xlrd import open_workbook
 
-path = 'test.xlsx'
-wb = open_workbook(path) # Open excel
-numHead = 0 # Number of headers
-KW = ['BCF ID', 'BTS ID', 'BTS NAME', 'SEG NAME', 'CI', 'BAND', 'NCC', 'BCC', 'MCC', 'MNC', 'LAC', 'Hopping Mode', 'HSN 1', 'HSN 2'] # Parameters key words
+path = 'test.xlsx' #Excel file path
+KW = ['BCF ID', 'BTS ID', 'BTS NAME', 'SEG NAME', 'CI', 'BAND', 'NCC', 'BCC', 'MCC', 'MNC', 'LAC', 'Hopping Mode', 'HSN 1', 'HSN 2'] # Parameter key words
 rowKW = 1 # Line to search key works
-tmp = -1
+iniValue = -1 #Initial value of array
 
 class Sheet:
     def __init__(self, r, c): # Initialization
         self.data = [[0 for x in range(c)] for x in range(r)] # Data handler
-        self.keyWords = [[tmp for x in range(len(KW))] for x in range(2)] # Parameters and index
+        self.keyWords = [[iniValue for x in range(len(KW))] for x in range(2)] # Parameters and index
+        self.numHead = 0 # Number of headers
 
         for i in range(len(self.keyWords)): # Fill array with parameters
             for j in range(len(self.keyWords[i])):
@@ -22,14 +21,12 @@ class Sheet:
         return self.getNumHead(value)
             
     def getNumHead(self, value): # Check number of headers
-        if numHead != 0:
-            return numHead
+        if self.numHead != 0:
+            return self.numHead
         else:
-            nHead = 0
             if c == 0:
                 if self.hasNumbers(value):
-                    nHead = r
-            return nHead
+                    self.numHead = r
     
     def hasNumbers(self, inputString): # Check if contains digit
         return any(char.isdigit() for char in inputString)
@@ -45,27 +42,40 @@ class Sheet:
 
     def printMML(self, sheet):
         if sheet == 'Site_BTS':
-            for j in range(numHead, len(self.data)): # Loop through data rows
+            for j in range(self.numHead, len(self.data)): # Loop through data rows
                 #print (self.keyWords)
                 #print (self.data[j][self.keyWords[1][5]])
-                band = self.GetFreqBand(str(self.data[j][self.keyWords[1][5]]))
-                hop = self.GetHopMode(str(self.data[j][self.keyWords[1][11]]))
                 
-                print ("ZEQC:BCF=" + str(int(self.data[j][self.keyWords[1][0]]))
-                + ",BTS=" + str(int(self.data[j][self.keyWords[1][1]]))
-                + ",NAME=" + str(self.data[j][self.keyWords[1][2]])
-                + ",SEGNAME=" + str(self.data[j][self.keyWords[1][3]]) # // REVIEW IF NEEDED
-                + ":CI=" + str(int(self.data[j][self.keyWords[1][4]]))
-                + ",BAND=" + str(band)
-                + ":NCC=" + str(int(self.data[j][self.keyWords[1][6]]))
-                + ",BCC=" + str(int(self.data[j][self.keyWords[1][7]]))
-                + ":MCC=" + str(int(self.data[j][self.keyWords[1][8]]))
-                + ",MNC=" + str(int(self.data[j][self.keyWords[1][9]]))
-                + ",LAC=" + str(int(self.data[j][self.keyWords[1][10]]))
-                + ":HOP=" + str(hop)
-                + ",HSN1=" + str(int(self.data[j][self.keyWords[1][12]]))
-                + ",HSN2=" + str(int(self.data[j][self.keyWords[1][13]]))
-                + ";")
+                           
+                output = "ZEQC:BCF=" + str(int(self.data[j][self.keyWords[1][0]]))
+                output += ",BTS=" + str(int(self.data[j][self.keyWords[1][1]]))
+                output += ",NAME=" + str(self.data[j][self.keyWords[1][2]])
+                output += ",SEGNAME=" + str(self.data[j][self.keyWords[1][3]]) # // REVIEW IF NEEDED
+                
+                output += ":CI=" + str(int(self.data[j][self.keyWords[1][4]]))
+                band = self.GetFreqBand(str(self.data[j][self.keyWords[1][5]]))
+                output += ",BAND=" + str(band)
+                
+                output += ":NCC=" + str(int(self.data[j][self.keyWords[1][6]]))
+                output += ",BCC=" + str(int(self.data[j][self.keyWords[1][7]]))
+                
+                output += ":MCC=" + str(int(self.data[j][self.keyWords[1][8]]))
+                output += ",MNC=" + str(int(self.data[j][self.keyWords[1][9]]))               
+                output += ",LAC=" + str(int(self.data[j][self.keyWords[1][10]]))
+                
+                hop = self.GetHopMode(str(self.data[j][self.keyWords[1][11]]))
+                output += ":HOP=" + str(hop)
+                
+                hsn1 = self.keyWords[1][12]
+                if hsn1 != iniValue:
+                    output += ",HSN1=" + str(int(self.data[j][hsn1]))
+                
+                hsn2 = self.keyWords[1][13]
+                if hsn2 != iniValue:
+                    output += ",HSN2=" + str(int(self.data[j][hsn2]))
+                
+                output += ";"
+                print(output)
                 
     def GetFreqBand(self, value): # Get frequency band
         if value == "": return "[!!!]"
@@ -80,16 +90,18 @@ class Sheet:
         else:
             hop = "[!!!]"
         return hop
-    
+
+        
+wb = open_workbook(path) # Open excel        
 for i in wb.sheets(): # Loop through sheets
     print ("Sheet: " + i.name + ", Rows: " + repr(i.nrows) + ", Cols: " + repr(i.ncols))   
     data = Sheet(i.nrows, i.ncols)
     
     for r in range(i.nrows): # Loop through rows
         for c in range(i.ncols):  # Loop through columns
-            numHead = data.readValue(r, c, i.cell(r, c).value)
+            data.readValue(r, c, i.cell(r, c).value)
     
-        if data.keyWords[1][0] == tmp: # Get parameters indexes
+        if data.keyWords[1][0] == iniValue: # Get parameters indexes
             data.kwSearch(i.name)
             
     data.printMML(i.name)
